58a3e3e7ef2275e6e6a4af8b6222702e1b219087
cai <1250653250@qq.com>
2021-07-21 01:58:48 +0800

    完成删除和修改功能

--- app/forms/posts.py
+++ app/forms/posts.py
@@ -1,10 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import TextAreaField, SubmitField, StringField
+from wtforms import TextAreaField, SubmitField, StringField, IntegerField
 from wtforms.validators import DataRequired, Length
 
 
 class PostsForm(FlaskForm):
     # 设置字段的其他属性使用render_kw完成
+    id = IntegerField("", render_kw={"style": "display:none"})
     title = StringField('', validators=[DataRequired(), Length(5, 100, message='标题长度请介于5至100之间')],
                         render_kw={"placeholder": '文章的标题'})
     content = TextAreaField(render_kw={'placeholder': '请输入文章内容', 'rows': '24', 'style': 'resize:none'},
--- app/views/article.py
+++ app/views/article.py
@@ -3,6 +3,7 @@ from app.forms import PostsForm
 from flask_login import login_required, current_user
 from app.models import Posts
 from app.extensions import db
+from datetime import datetime
 
 article = Blueprint('article', __name__)
 
@@ -38,3 +39,40 @@ def article_detail():
     id = request.args.get('id')
     posts = Posts.query.filter_by(id=id).first()
     return render_template('article/article_detail.html', posts=posts)
+
+
+@article.route('/article_delete', methods=['GET'])
+@login_required
+def article_delete():
+    id = request.args.get('id')
+    result = Posts.query.filter_by(id=id).delete()
+    if result is not None:
+        return redirect(url_for('article.mypublish'))
+
+
+@article.route('/article_change', methods=['GET', 'POST'])
+@login_required
+def article_change():
+    form = PostsForm()
+    if request.method == 'GET':
+        id = request.args.get('id')
+        posts = Posts.query.filter_by(id=id).first()
+        form.id.data = posts.id
+        form.title.data = posts.title
+        form.content.data = posts.content
+        return render_template('article/publish.html', form=form)
+    else:
+        if form.validate_on_submit():
+            id = Posts.query.filter_by(id=form.id.data).update(
+                {"title": form.title.data, "content": form.content.data, "describe": form.content.data[0:145] + '……',
+                 "timestamp": datetime.utcnow()})
+            return redirect(url_for("article.mypublish"))
+
+
+@article.route('/article_search', methods=['GET'])
+def article_search():
+    keyword = request.args.get('keyword')
+    page = request.args.get('page', 1, type=int)
+    pagination = Posts.query.filter(Posts.title.like('%keyword%'.format(keyword=keyword))).order_by(Posts.timestamp.desc()).paginate(page, per_page=5)
+    posts = pagination.items
+    return render_template('main/index.html', posts=posts, pagination=pagination)
